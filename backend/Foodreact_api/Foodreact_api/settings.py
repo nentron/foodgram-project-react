01c063b5145d05e3c8cc515b530c76cb72308f2d
Bobko Sergei <sergobob3@mail.ru>
2023-02-01 11:54:54 -0800

    redoc added

--- backend/Foodreact_api/Foodreact_api/settings.py
+++ backend/Foodreact_api/Foodreact_api/settings.py
@@ -50,7 +50,7 @@ ROOT_URLCONF = 'Foodreact_api.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- backend/Foodreact_api/Foodreact_api/urls.py
+++ backend/Foodreact_api/Foodreact_api/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
 from rest_framework.routers import DefaultRouter
+from rest_framework.schemas import get_schema_view
 
 from users.views import (
     UserViewSet, TokenView,
@@ -10,6 +11,9 @@ from recipes.views import (
     IngredientViewset,
     TagViewset, RecipeViewset
 )
+
+
+schema_view = get_schema_view(title='FoodGram')
 
 
 router = DefaultRouter()
@@ -25,8 +29,16 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
     path(
+        'openapi',
+        schema_view,
+        name='openapi-schema'
+    ),
+    path(
         'api/docs/',
-        TemplateView.as_view(template_name='redoc.html'),
+        TemplateView.as_view(
+            template_name='redoc.html',
+            extra_context={'schema_url': 'openapi-schema'}
+        ),
         name='redoc'
     )
 ]
